ecf2b4c67d679f8528ce39e5c343b4eed6e342b5
LeonardoCabrini06 <leonardo.mauro@abacobr.consulting>
2026-07-02 15:40:47 -0300

    Implementação de Logica de Inativação de Produto

--- webapp/controller/MM/ViewProducts.controller.js
+++ webapp/controller/MM/ViewProducts.controller.js
@@ -8,7 +8,7 @@ sap.ui.define(
     "sap/ui/core/Fragment",
     "sap/ui/model/json/JSONModel",
     "sap/m/MessageToast",
-    "sap/m/MessageBox"
+    "sap/m/MessageBox",
   ],
   (
     Controller,
@@ -19,7 +19,7 @@ sap.ui.define(
     Fragment,
     JSONModel,
     MessageToast,
-    MessageBox
+    MessageBox,
   ) => {
     "use strict";
 
@@ -29,9 +29,12 @@ sap.ui.define(
       onInit() {
         this._oCustomStatus = this.byId("mcb_status");
         this._oCustomCategory = this.byId("mcb_category");
-        this._oSmartTableProducts = this.byId("ProductsSmartTable")
+        this._oSmartTableProducts = this.byId("ProductsSmartTable");
 
-        this.getView().setModel(new JSONModel(this._getCreateProductInitialData()), "createProduct");
+        this.getView().setModel(
+          new JSONModel(this._getCreateProductInitialData()),
+          "createProduct",
+        );
 
         this.getOwnerComponent()
           .getRouter()
@@ -46,7 +49,7 @@ sap.ui.define(
         }
       },
 
-      onResetFilter:function(){
+      onResetFilter: function () {
         this._oCustomStatus.setSelectedKeys(["A"]);
         this._oCustomCategory.setSelectedKeys([]);
 
@@ -120,64 +123,53 @@ sap.ui.define(
       },
 
       onOpenCreateDialog: function () {
-
-        this._resetCreateProductModel()
+        this._resetCreateProductModel();
 
         if (!this._pCreateDialog) {
-
           this._pCreateDialog = Fragment.load({
-
             id: this.getView().getId(),
             name: "zcoffee.view.fragments.AddProduct",
             controller: this,
           }).then((oDialog) => {
-
             this.getView().addDependent(oDialog);
             return oDialog;
           });
         }
 
         this._pCreateDialog.then((oDialog) => {
-
           oDialog.open();
         });
       },
 
-      onSaveProduct: function(){
-
-        const oDataModel = this.getView().getModel(); 
+      onSaveProduct: function () {
+        const oDataModel = this.getView().getModel();
         const oPayload = this._montaPayload();
 
-        if(!this._validarCampos(oPayload)){
+        if (!this._validarCampos(oPayload)) {
           return;
         }
 
         console.log(oPayload);
-        
-        oDataModel.create("/ZCDS_PRODUTOS", oPayload,{
 
-          success:function(oDataNew){
-
-            MessageBox.success(`Produto ${oDataNew.IdProduto} criado com sucesso!`)
+        oDataModel.create("/ZCDS_PRODUTOS", oPayload, {
+          success: function (oDataNew) {
+            MessageBox.success(
+              `Produto ${oDataNew.IdProduto} criado com sucesso!`,
+            );
 
             this._resetCreateProductModel();
             this.byId("createProductDialog").close();
             this._oSmartTableProducts.rebindTable();
-
           }.bind(this),
 
-          error:function(oError){
-
+          error: function (oError) {
             console.log(oError);
             MessageBox.error("Falha ao criar Produto!");
-          }
-
-        })
-
+          },
+        });
       },
 
-      _montaPayload:function(){
-
+      _montaPayload: function () {
         const oData = this.getView().getModel("createProduct").getData();
         const oDatePicker = this.byId("dp2").getDateValue();
 
@@ -190,33 +182,27 @@ sap.ui.define(
           Quantidade: oData.Quantidade,
           Status: oData.Status,
           Validade: oDatePicker,
-          ImagemURL: oData.ImagemURL
-        }
-
+          ImagemURL: oData.ImagemURL,
+        };
       },
 
-      _validarCampos:function(oData){
-
+      _validarCampos: function (oData) {
         if (!oData.Nome) {
-
           MessageToast.show("Preencha o corretamente o Nome!");
           return false;
         }
 
         if (!oData.Preco || oData.Preco <= 0) {
-
           MessageToast.show("Preencha corretamente o Preço!");
           return false;
         }
 
         if (oData.Quantidade < 0) {
-
           MessageBox.error("Quantidade inválida.");
           return false;
         }
 
         if (!oData.Validade) {
-
           MessageBox.error("Data de Validade inválida.");
           return false;
         }
@@ -227,35 +213,76 @@ sap.ui.define(
       onCloseDialog: function () {
         this._resetCreateProductModel();
         this.byId("createProductDialog").close();
-    },
+      },
 
-    _getCreateProductInitialData:function(){
+      _getCreateProductInitialData: function () {
+        return {
+          Categoria: "1",
+          Status: "A",
+          Moeda: "BRL",
+          Quantidade: 1,
+        };
+      },
 
-      return {
-        Categoria:"1",
-        Status:"A",
-        Moeda:"BRL",
-        Quantidade:1
-      }
-    },
+      _resetCreateProductModel: function () {
+        this.getView()
+          .getModel("createProduct")
+          .setData(this._getCreateProductInitialData());
+      },
 
-    _resetCreateProductModel:function(){
-      this.getView().getModel("createProduct").setData(this._getCreateProductInitialData())
-    },
+      onPrecoChange: function (oEvent) {
+        const oInput = oEvent.getSource();
+        let sValue = oInput.getValue();
 
-    onPrecoChange: function (oEvent) {
+        sValue = parseFloat(sValue.replace(",", "."));
 
-      const oInput = oEvent.getSource();
-      let sValue = oInput.getValue();
-
-      sValue = parseFloat(sValue.replace(",", "."));
-
-      if (!isNaN(sValue)) {
+        if (!isNaN(sValue)) {
           oInput.setValue(sValue.toFixed(2));
-      }
+        }
+      },
 
-    }
+      onDelete: function (oEvent) {
+        const sId = oEvent
+          .getParameter("listItem")
+          .getBindingContext()
+          .getPath()
+          .split("'")[1];
 
+        const oModel = this.getView().getModel();
+
+        oModel.read(`/ZCDS_PRODUTOS('${sId}')`, {
+
+          success: (oData) => {
+
+            if (oData.Status !== "A") {
+
+              MessageBox.error(`Produto ${sId} já está inativado.`);
+              return;
+            }
+
+            oModel.remove(`/ZCDS_PRODUTOS('${sId}')`, {
+
+              success: () => {
+
+                MessageBox.success(`Produto ${sId} inativado com sucesso!`);
+                this._oSmartTableProducts.rebindTable();
+              },
+
+              error: (oError) => {
+
+                console.log(oError);
+                MessageBox.error(`Falha ao inativar produto ${sId}.`);
+              },
+            });
+          },
+
+          error: (oError) => {
+            
+            console.log(oError);
+            MessageBox.error("Erro ao consultar o produto.");
+          },
+        });
+      },
     });
   },
 );
